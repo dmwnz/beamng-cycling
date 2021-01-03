7e57c5e6236bd66aca7d50a0f8eb96fe24100b86
Damien <damien.wintz@gmail.com>
2021-01-03 19:32:07 +0100

    driveable bike

--- Proto/BeamNG.drive__mods__unpacked/sir_velo/vehicles/sir_velo/lua/sim_cycling.lua
+++ Proto/BeamNG.drive__mods__unpacked/sir_velo/vehicles/sir_velo/lua/sim_cycling.lua
@@ -10,8 +10,6 @@ local targetSpeed= 0
 local throttleSmooth = newTemporalSmoothing(200, 200)
 local speedPID = newPIDStandard(0.3, 2, 0.0, 0, 1, 1, 1, 0, 2)
 local crank_rotation = 0  -- degrees
-local wheel_rotation = 0  -- degrees
-local fake_steering = 0
 
 
 local function setSpeed(speed)
@@ -20,10 +18,8 @@ local function setSpeed(speed)
 end
 
 local function onInit()
-    electrics.values.fake_steering = 0
     electrics.values.throttle = 0
     electrics.values.crank_rotation = crank_rotation
-    electrics.values.wheel_rotation = wheel_rotation
     -- local result, error = client:connect(host, port);
     -- if result == nil then
     --     log('E', 'sim_cycling', error)
@@ -56,11 +52,7 @@ local function updateGFX(dt)
     --readCompanionData()
     --cruiseControl(dt)
     crank_rotation = (crank_rotation + electrics.values.throttle_input) % 360
-    wheel_rotation = (wheel_rotation + electrics.values.throttle_input * 2.5) % 360
-    
     electrics.values.crank_rotation = crank_rotation
-    electrics.values.wheel_rotation = wheel_rotation
-    electrics.values.fake_steering = electrics.values.steering_input
     -- log('I', 'sim_cycling', electrics.values['crank_rotation'])
 end
 
